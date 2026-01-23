08e9bf90b2de9d4766f0afe2febfbd77a97c5ec6
opalaabl <web3.opal@gmail.com>
2026-01-23 19:09:26 +0700

    chore: remove build artifacts from repository

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gu2TfsMCDodsEYTeiYPto2W3uCjDJXBJnsyfvG+U8N4=\"\n}"
--- .next/dev/static/chunks/0e3a9_@noble_curves_esm_secp256k1_3f0cd917.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/0e3a9_@noble_curves_esm_c030fadf._.js"
-],
-    source: "dynamic"
-});
